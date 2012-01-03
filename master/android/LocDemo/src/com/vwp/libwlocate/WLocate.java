8e287e90fdef0e386350c56259ba40c3d5084937
oxy <oxygenic@users.sourceforge.net>
2012-01-03 12:56:01 +0100

    documentation added

--- master/android/LocDemo/src/com/vwp/libwlocate/WLocate.java
+++ master/android/LocDemo/src/com/vwp/libwlocate/WLocate.java
@@ -10,6 +10,9 @@ import android.os.*;
 
 
 
+/**
+ * Internal class, used for storing request data
+ */
 class wloc_req
 {
    public static final int WLOC_MAX_NETWORKS=16;
@@ -29,6 +32,9 @@ class wloc_req
 
 
 
+/**
+ * Internal class, used for receiving the result
+ */
 class wloc_res
 {
    public byte  version,length;
@@ -42,6 +48,9 @@ class wloc_res
 
 
 
+/**
+ * Internal class, used for storing the position information
+ */
 class wloc_position
 {
    wloc_position()
@@ -55,13 +64,22 @@ class wloc_position
 
 
 
+/**
+ * Geopositioning/location class to evaluate the current position without using the standard location mechanisms
+ * where the privacy rules are not clear. It tries to evaluate the current geographic position by using GPs and
+ * - in case this fails or GPS is not available - by using other parameters like surrounding WLAN networks.<BR><BR>
+ * 
+ * The usage is quite simple: create an own class that inherits from WLocate and overwrite method
+ * wloc_return_position(). Call wloc_request_position() to start position evaluation. The resulting is returned via
+ * overwritten method wloc_return_position() asynchronously.
+ */
 public class WLocate
 {
-   public static final int WLOC_OK=0;
-   public static final int WLOC_CONNECTION_ERROR=1;
-   public static final int WLOC_SERVER_ERROR=2;
-   public static final int WLOC_LOCATION_ERROR=3;
-   public static final int WLOC_ERROR=100;
+   public static final int WLOC_OK=0;               /** Result code for position request, given position information are OK */
+   public static final int WLOC_CONNECTION_ERROR=1; /** Result code for position request, a connection error occured, no position information are available */
+   public static final int WLOC_SERVER_ERROR=2;     
+   public static final int WLOC_LOCATION_ERROR=3;   /** Result code for position request, the position could not be evaluated, no position information are available */
+   public static final int WLOC_ERROR=100;          /** Result code for position request, an unknown error occured, no position information are available */
    
    private static final int WLOC_RESULT_OK=1;
    private static final int WLOC_RESULT_ERROR=2;
@@ -76,7 +94,11 @@ public class WLocate
    private float               m_radius=1.0f;
 
    
-   
+
+   /**
+    * Constructor for WLocate class, this constructor has to be overwritten by inheriting class
+    * @param ctx current context, hand over Activity object here
+    */
    public WLocate(Context ctx)
    {
       location= (LocationManager)ctx.getSystemService(Context.LOCATION_SERVICE);
@@ -89,6 +111,10 @@ public class WLocate
    
    
    
+   /**
+    * Start position evaluation process, the result is returned via method wloc_return_position()
+    * that may be called asynchronously
+    */
    public void wloc_request_position()
    {
       if (!GPSAvailable) wifi.startScan();
@@ -96,366 +122,6 @@ public class WLocate
    }
    
 
-   
-   public String wloc_get_country_from_code(short ccode)
-   {
-      switch (ccode)
-      {
-         case 1:
-            return "DE";
-         case 2:
-            return "AT";
-         case 3:
-            return "CH";
-         case 4:
-            return "NL";
-         case 5:
-            return "BE";
-         case 6:
-            return "LU";
-
-         case 7:
-            return "NO";
-
-         case 8:
-            return "SE";
-
-         case 9:
-            return "DK";
-
-         case 10:
-            return "AF";
-
-         case 12:
-            return "AL";
-
-         case 13:
-            return "DZ";
-
-         case 17:
-            return "AN";
-
-         case 18:
-            return "AG";
-
-         case 19:
-            return "AR";
-
-         case 20:
-            return "AM";
-
-         case 21:
-            return "AU";
-
-         case 23:
-            return "BS";
-
-         case 24:
-            return "BH";
-
-         case 25:
-            return "BD";
-
-         case 26:
-            return "BB";
-
-         case 27:
-            return "BY";
-
-         case 28:
-            return "BZ";
-
-         case 29:
-            return "BJ";
-
-         case 30:
-            return "BM";
-
-         case 32:
-            return "BO";
-
-         case 33:
-            return "BA";
-
-         case 36:
-            return "BR";
-
-         case 37:
-            return "BN";
-
-         case 38:
-            return "BG";
-
-         case 43:
-            return "CA";
-
-         case 44:
-            return "CV";
-
-         case 47:
-            return "CL";
-
-         case 48:
-            return "CN";
-         case 49:
-            return "CO";
-         case 52:
-            return "CR";
-         case 53:
-            return "HR";
-         case 55:
-            return "CY";
-         case 56:
-            return "CZ";
-         case 59:
-            return "DO";
-         case 60:
-            return "EC";
-         case 61:
-            return "EG";
-         case 66:
-            return "ET";
-         case 68:
-            return "FI";
-         case 69:
-            return "FR";
-         case 73:
-            return "GH";
-         case 75:
-            return "GR";
-
-         case 76:
-            return "GL";
-
-         case 77:
-            return "GD";
-
-         case 78:
-            return "GU";
-
-         case 79:
-            return "GT";
-
-         case 82:
-            return "HT";
-
-         case 83:
-            return "HN";
-
-         case 84:
-            return "HK";
-
-         case 85:
-            return "HU";
-
-         case 86:
-            return "IS";
-
-         case 87:
-            return "IN";
-
-         case 88:
-            return "ID";
-
-         case 89:
-            return "IR";
-
-         case 90:
-            return "IQ";
-
-         case 91:
-            return "IE";
-
-         case 93:
-            return "IT";
-
-         case 94:
-            return "JM";
-
-         case 95:
-            return "JP";
-
-         case 97:
-            return "JO";
-
-         case 98:
-            return "KZ";
-
-         case 99:
-            return "KE";
-
-         case 102:
-            return "KR";
-
-         case 103:
-            return "KW";
-
-         case 104:
-            return "KG";
-
-         case 105:
-            return "LA";
-
-         case 106:
-            return "LV";
-
-         case 107:
-            return "LB";
-
-         case 108:
-            return "LS";
-
-         case 111:
-            return "LT";
-
-         case 115:
-            return "MY";
-
-         case 116:
-            return "MV";
-
-         case 118:
-            return "MT";
-
-         case 119:
-            return "MQ";
-
-         case 121:
-            return "MU";
-
-         case 123:
-            return "MX";
-
-         case 124:
-            return "MC";
-
-         case 125:
-            return "MN";
-
-         case 126:
-            return "MA";
-
-         case 127:
-            return "MZ";
-
-         case 131:
-            return "NZ";
-
-         case 133:
-            return "NI";
-
-         case 135:
-            return "NG";
-
-         case 137:
-            return "OM";
-
-         case 138:
-            return "PK";
-
-         case 141:
-            return "PA";
-
-         case 142:
-            return "PY";
-
-         case 144:
-            return "PE";
-
-         case 145:
-            return "PH";
-
-         case 147:
-            return "PL";
-
-         case 148:
-            return "PT";
-
-         case 149:
-            return "PR";
-
-         case 150:
-            return "QA";
-
-         case 151:
-            return "RO";
-
-         case 152:
-            return "RU";
-
-         case 155:
-            return "SM";
-
-         case 157:
-            return "SA";
-
-         case 158:
-            return "SN";
-
-         case 161:
-            return "SG";
-
-         case 162:
-            return "SK";
-
-         case 163:
-            return "SI";
-
-         case 166:
-            return "ZA";
-
-         case 167:
-            return "ES";
-
-         case 168:
-            return "LK";
-
-         case 169:
-            return "SD";
-         case 170:
-            return "SR";
-         case 172:
-            return "SY";
-         case 173:
-            return "TW";
-         case 174:
-            return "TJ";
-         case 175:
-            return "TZ";
-         case 176:
-            return "TH";
-         case 179:
-            return "TT";
-         case 180:
-            return "TN";
-         case 181:
-            return "TR";
-         case 182:
-            return "TM";
-         case 185:
-            return "UA";
-         case 186:
-            return "AE";
-         case 187:
-            return "UK";
-         case 188:
-            return "US";
-         case 189:
-            return "UY";
-         case 191:
-            return "VE";
-         case 192:
-            return "VN";
-         case 195:
-            return "ZM";
-         case 196:
-            return "ZW";
-         default:
-            return null;
-      }
-   }
-   
-   
    
    private int get_position(wloc_req request,wloc_position position)
    {
@@ -546,16 +212,307 @@ public class WLocate
          ret=get_position(request,pos);
          if (pos.quality<=0) wloc_return_position(ret,pos.lat,pos.lon,10000.0f,pos.ccode);
          else wloc_return_position(ret,pos.lat,pos.lon,120-pos.quality,pos.ccode);
-     }
-  }
+      }
+   }
    
    
+   
+   /**
+    * This method is called as soon as a result of a position evaluation request is available.
+    * Thus this method should be overwritten by the inheriting class to receive the results there.
+    * @param ret the return code that informs if the location evaluation request could be fulfilled
+    *        successfully or not. Only in case this parameter is equal to WLOC_OK all the other
+    *        ones can be used, elsewhere no position information could be retrieved.
+    * @param lat the latitude of the current position 
+    * @param lon the latitude of the current position 
+    * @param radius the accuracy of the position information, this radius specifies the range around
+    *        the given latitude and longitude information of the real position. The smaller this value
+    *        is the more accurate the given position information is.
+    * @param ccode code of the country where the current position is located within, in case the
+    *        country is not known, 0 is returned. The country code can be converted to a text that
+    *        specifies the country by calling wloc_get_country_from_code()
+    */
    protected void wloc_return_position(int ret,double lat,double lon,float radius,short ccode)
    {
       
    }
+
    
    
+   /**
+    * Convert a country code to a more easy to read short text that specifies a country.
+    * @param ccode the country code to be converted
+    * @return the short text that names the country or an empty string an unknown country
+    *         specifier was given or the country code was 0
+    */
+   public String wloc_get_country_from_code(short ccode)
+   {
+      switch (ccode)
+      {
+         case 1:
+            return "DE";
+         case 2:
+            return "AT";
+         case 3:
+            return "CH";
+         case 4:
+            return "NL";
+         case 5:
+            return "BE";
+         case 6:
+            return "LU";
+         case 7:
+            return "NO";
+         case 8:
+            return "SE";
+         case 9:
+            return "DK";
+         case 10:
+            return "AF";
+         case 12:
+            return "AL";
+         case 13:
+            return "DZ";
+         case 17:
+            return "AN";
+         case 18:
+            return "AG";
+         case 19:
+            return "AR";
+         case 20:
+            return "AM";
+         case 21:
+            return "AU";
+         case 23:
+            return "BS";
+         case 24:
+            return "BH";
+         case 25:
+            return "BD";
+         case 26:
+            return "BB";
+         case 27:
+            return "BY";
+         case 28:
+            return "BZ";
+         case 29:
+            return "BJ";
+         case 30:
+            return "BM";
+         case 32:
+            return "BO";
+         case 33:
+            return "BA";
+         case 36:
+            return "BR";
+         case 37:
+            return "BN";
+         case 38:
+            return "BG";
+         case 43:
+            return "CA";
+         case 44:
+            return "CV";
+         case 47:
+            return "CL";
+         case 48:
+            return "CN";
+         case 49:
+            return "CO";
+         case 52:
+            return "CR";
+         case 53:
+            return "HR";
+         case 55:
+            return "CY";
+         case 56:
+            return "CZ";
+         case 59:
+            return "DO";
+         case 60:
+            return "EC";
+         case 61:
+            return "EG";
+         case 66:
+            return "ET";
+         case 68:
+            return "FI";
+         case 69:
+            return "FR";
+         case 73:
+            return "GH";
+         case 75:
+            return "GR";
+         case 76:
+            return "GL";
+         case 77:
+            return "GD";
+         case 78:
+            return "GU";
+         case 79:
+            return "GT";
+         case 82:
+            return "HT";
+         case 83:
+            return "HN";
+         case 84:
+            return "HK";
+         case 85:
+            return "HU";
+         case 86:
+            return "IS";
+         case 87:
+            return "IN";
+         case 88:
+            return "ID";
+         case 89:
+            return "IR";
+         case 90:
+            return "IQ";
+         case 91:
+            return "IE";
+         case 93:
+            return "IT";
+         case 94:
+            return "JM";
+         case 95:
+            return "JP";
+         case 97:
+            return "JO";
+         case 98:
+            return "KZ";
+         case 99:
+            return "KE";
+         case 102:
+            return "KR";
+         case 103:
+            return "KW";
+         case 104:
+            return "KG";
+         case 105:
+            return "LA";
+         case 106:
+            return "LV";
+         case 107:
+            return "LB";
+         case 108:
+            return "LS";
+         case 111:
+            return "LT";
+         case 115:
+            return "MY";
+         case 116:
+            return "MV";
+         case 118:
+            return "MT";
+         case 119:
+            return "MQ";
+         case 121:
+            return "MU";
+         case 123:
+            return "MX";
+         case 124:
+            return "MC";
+         case 125:
+            return "MN";
+         case 126:
+            return "MA";
+         case 127:
+            return "MZ";
+         case 131:
+            return "NZ";
+         case 133:
+            return "NI";
+         case 135:
+            return "NG";
+         case 137:
+            return "OM";
+         case 138:
+            return "PK";
+         case 141:
+            return "PA";
+         case 142:
+            return "PY";
+         case 144:
+            return "PE";
+         case 145:
+            return "PH";
+         case 147:
+            return "PL";
+         case 148:
+            return "PT";
+         case 149:
+            return "PR";
+         case 150:
+            return "QA";
+         case 151:
+            return "RO";
+         case 152:
+            return "RU";
+         case 155:
+            return "SM";
+         case 157:
+            return "SA";
+         case 158:
+            return "SN";
+         case 161:
+            return "SG";
+         case 162:
+            return "SK";
+         case 163:
+            return "SI";
+         case 166:
+            return "ZA";
+         case 167:
+            return "ES";
+         case 168:
+            return "LK";
+         case 169:
+            return "SD";
+         case 170:
+            return "SR";
+         case 172:
+            return "SY";
+         case 173:
+            return "TW";
+         case 174:
+            return "TJ";
+         case 175:
+            return "TZ";
+         case 176:
+            return "TH";
+         case 179:
+            return "TT";
+         case 180:
+            return "TN";
+         case 181:
+            return "TR";
+         case 182:
+            return "TM";
+         case 185:
+            return "UA";
+         case 186:
+            return "AE";
+         case 187:
+            return "UK";
+         case 188:
+            return "US";
+         case 189:
+            return "UY";
+         case 191:
+            return "VE";
+         case 192:
+            return "VN";
+         case 195:
+            return "ZM";
+         case 196:
+            return "ZW";
+         default:
+            return "";
+      }
+   }
+   
+    
    
    class GPSLocationListener implements LocationListener 
    {
@@ -585,3 +542,5 @@ public class WLocate
    };
    
 }
+
+
