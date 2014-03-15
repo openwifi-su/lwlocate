8d621f2126a47c04f7afb68c4516004a966e444a
universe <universe@localhost.localdomain>
2014-03-15 19:54:38 +0100

    optionally disable HTTPS upload

--- master/android/OWMiniAtAndroid/src/com/vwp/owmini/UploadThread.java
+++ master/android/OWMiniAtAndroid/src/com/vwp/owmini/UploadThread.java
@@ -2,6 +2,7 @@ package com.vwp.owmini;
 
 import android.content.*;
 import android.net.*;
+import android.preference.PreferenceManager;
 import android.app.*;
 
 import java.io.*;
@@ -10,6 +11,7 @@ import java.util.*;
 import java.security.*;
 import java.security.cert.*;
 import java.security.cert.Certificate;
+
 import javax.net.ssl.*;
 
 import com.vwp.owmini.OWMiniAtAndroid.*;
@@ -24,7 +26,7 @@ class UploadThread extends Thread
    private Notification        notification;
    private NetworkInfo         mWifi;
 
-   private static final int    version=105;
+   private static final int    version=107;
    private static final String FILE_UPLOADSTORE="uploadstore";
  
 
@@ -263,133 +265,232 @@ class UploadThread extends Thread
    
    private boolean uploadData(String outString,boolean silent)
    {
-      HttpsURLConnection   c=null;
-      BufferedOutputStream os=null;
-      DataInputStream      is=null;
-      int                  rc,remoteVersion=0;
-      String               inString;         
-      int                  newAPs=0,updAPs=0,delAPs=0,newPoints=0;
-      boolean              uploadSuccess=true;
-
-      if ((silent) && (mWifi!=null))
-      {
-         if (!mWifi.isConnected()) return false;
-      }
+      SharedPreferences SP;
+      boolean           uploadSuccess=true;
+      int               newAPs=0,updAPs=0,delAPs=0,newPoints=0;
       
-      try
+      SP = PreferenceManager.getDefaultSharedPreferences(ctx.getBaseContext());
+      if (SP.getBoolean("httpUpload",false))
       {
-      	// as described at http://developer.android.com/training/articles/security-ssl.html
-      	// Load CAs from an InputStream
-      	// (could be from a resource or ByteArrayInputStream or ...)
-      	CertificateFactory cf = CertificateFactory.getInstance("X.509");
-      	// From https://www.washington.edu/itconnect/security/ca/load-der.crt
-      	InputStream caInput = new BufferedInputStream(ctx.getResources().openRawResource(R.raw.root));
-      	Certificate ca;
-      	try 
-      	{
-      	   ca = cf.generateCertificate(caInput);
-      	} 
-      	finally 
-      	{
-      	   caInput.close();
-      	}
-
-      	// Create a KeyStore containing our trusted CAs
-      	String keyStoreType = KeyStore.getDefaultType();
-      	KeyStore keyStore = KeyStore.getInstance(keyStoreType);
-      	keyStore.load(null, null);
-      	keyStore.setCertificateEntry("ca", ca);
-
-      	// Create a TrustManager that trusts the CAs in our KeyStore
-      	String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-      	TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-      	tmf.init(keyStore);
-
-      	// Create an SSLContext that uses our TrustManager
-      	SSLContext context = SSLContext.getInstance("TLS");
-      	context.init(null, tmf.getTrustManagers(), null);
-
-      	// Tell the URLConnection to use a SocketFactory from our SSLContext
-      	URL url = new URL("https://openwlanmap.org/android/upload.php");
-      	c =(HttpsURLConnection)url.openConnection();
-      	c.setSSLSocketFactory(context.getSocketFactory());    	
+         HttpURLConnection c=null;
+    	 BufferedOutputStream os=null;
+         DataInputStream is=null;
+         int rc,remoteVersion=0;
+         String inString;
          
-          c.setDoOutput(true); // enable POST
-          c.setRequestMethod("POST");
-          c.addRequestProperty("Content-Type","application/x-www-form-urlencoded, *.*");
-          c.addRequestProperty("Content-Length",""+outString.length());
-          os = new BufferedOutputStream(c.getOutputStream());
-          os.write(outString.getBytes(),0,outString.length());
-          os.flush();
-          os.close();
-          outString=null;
-          os=null;
-          System.gc();
-
-         rc = c.getResponseCode();
-         if (rc != HttpURLConnection.HTTP_OK) 
+    	 if ((silent) && (mWifi!=null))
          {
-            if (!silent)
-             OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getString(R.string.http_error)+" "+rc);
-            return false;
-         }         
-         is = new DataInputStream(c.getInputStream());         
-         try
+    	    if (!mWifi.isConnected()) return false;
+    	 }
+    	 try
          {
-            inString=is.readLine();
-            remoteVersion=Integer.parseInt(inString);
-            inString=is.readLine();
-            scanData.uploadedCount=Integer.parseInt(inString);
-            inString=is.readLine();
-            scanData.uploadedRank=Integer.parseInt(inString);
-
-            inString=is.readLine();
-            newAPs=Integer.parseInt(inString);
-            inString=is.readLine();
-            updAPs=Integer.parseInt(inString);
-            inString=is.readLine();
-            delAPs=Integer.parseInt(inString);
-            inString=is.readLine();
-            newPoints=Integer.parseInt(inString);
-         }
-         catch (NumberFormatException nfe)
+    	    URL connectURL = new URL("http://www.openwlanmap.org/android/upload.php");
+            c= (HttpURLConnection) connectURL.openConnection();
+    	    if (c==null) return false;
+    	    c.setDoOutput(true); // enable POST
+    	    c.setRequestMethod("POST");
+    	    c.addRequestProperty("Content-Type","application/x-www-form-urlencoded, *.*");
+    	    c.addRequestProperty("Content-Length",""+outString.length());
+    	    os = new BufferedOutputStream(c.getOutputStream());
+    	    os.write(outString.getBytes(),0,outString.length());
+    	    os.flush();
+    	    os.close();
+    	    outString=null;
+    	    os=null;
+    	    System.gc();
+    	    rc = c.getResponseCode();
+    	    if (rc != HttpURLConnection.HTTP_OK)
+    	    {
+    	       if (!silent)
+    	        OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getString(R.string.http_error)+" "+rc);
+    	       return false;
+    	    }
+    	    is = new DataInputStream(c.getInputStream());
+    	    try
+    	    {
+    	       inString=is.readLine();
+    	       remoteVersion=Integer.parseInt(inString);
+    	       inString=is.readLine();
+               scanData.uploadedCount=Integer.parseInt(inString);
+    	       inString=is.readLine();
+    	       scanData.uploadedRank=Integer.parseInt(inString);
+    	       inString=is.readLine();
+    	       newAPs=Integer.parseInt(inString);
+    	       inString=is.readLine();
+    	       updAPs=Integer.parseInt(inString);
+    	       inString=is.readLine();
+    	       delAPs=Integer.parseInt(inString);
+    	       inString=is.readLine();
+    	       newPoints=Integer.parseInt(inString);
+    	    }
+    	    catch (NumberFormatException nfe)
+    	    {
+    	       nfe.printStackTrace();
+    	       throw new IOException("upload.php failed");
+    	    }
+    	    is.close();
+    	    is=null;
+    	    ctx.deleteFile(OWMiniAtAndroid.WSCAN_FILE);
+    	    ctx.deleteFile(OWMiniAtAndroid.WFREI_FILE);
+    	    ctx.deleteFile("wopendata");
+    	    ScanService.scanData.setStoredValues(0);
+    	    ScanService.scanData.setFreeHotspotWLANs(0);
+    	    ctx.storeConfig();
+    	    if (!silent)
+    	    {
+    	       if (remoteVersion>version)
+    	       OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getText(R.string.new_version_available));
+    	    }
+    	 }
+    	 catch (IOException ioe)
          {
-            nfe.printStackTrace();
-            throw new IOException("upload.php failed");
-         }
-         
-         is.close();
-         is=null;
-         ctx.deleteFile(OWMiniAtAndroid.WSCAN_FILE);
-         ctx.deleteFile(OWMiniAtAndroid.WFREI_FILE);
-         ScanService.scanData.setStoredValues(0);
-         ScanService.scanData.setFreeHotspotWLANs(0);
-         ctx.storeConfig();
-         if (!silent)
+    	    if (!silent)
+    	     OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getText(R.string.upload_problem));
+    	    uploadSuccess=false;
+    	 }
+    	 finally
          {
-            if (remoteVersion>version)
-             OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getText(R.string.new_version_available));
+            try
+            {
+    	       if (is != null) is.close();
+    	       if (os != null) os.close();
+               if (c != null) c.disconnect();
+            }
+    	    catch (IOException ioe)
+            {
+    	       ioe.printStackTrace();
+    	    }
          }
       }
-      catch (Exception e)
+      else
       {
-         if (!silent)
-          OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getText(R.string.upload_problem)+"\r\n"+e.getLocalizedMessage());
-         uploadSuccess=false;
+	      HttpsURLConnection   c=null;
+	      BufferedOutputStream os=null;
+	      DataInputStream      is=null;
+	      int                  rc,remoteVersion=0;
+	      String               inString;         
+	
+	      if ((silent) && (mWifi!=null))
+	      {
+	         if (!mWifi.isConnected()) return false;
+	      }
+	      
+	      try
+	      {    	  
+	    	// as described at http://developer.android.com/training/articles/security-ssl.html
+	    	// Load CAs from an InputStream
+	    	// (could be from a resource or ByteArrayInputStream or ...)
+	    	CertificateFactory cf = CertificateFactory.getInstance("X.509");
+	    	// From https://www.washington.edu/itconnect/security/ca/load-der.crt
+	    	InputStream caInput = new BufferedInputStream(ctx.getResources().openRawResource(R.raw.root));
+	    	Certificate ca;
+	    	try 
+	    	{
+	    	   ca = cf.generateCertificate(caInput);
+	    	} 
+	    	finally 
+	    	{
+	    	   caInput.close();
+	    	}
+	
+	    	// Create a KeyStore containing our trusted CAs
+	    	String keyStoreType = KeyStore.getDefaultType();
+	    	KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+	    	keyStore.load(null, null);
+	    	keyStore.setCertificateEntry("ca", ca);
+	
+	    	// Create a TrustManager that trusts the CAs in our KeyStore
+	    	String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+	    	TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+	    	tmf.init(keyStore);
+	
+	    	// Create an SSLContext that uses our TrustManager
+	    	SSLContext context = SSLContext.getInstance("TLS");
+	    	context.init(null, tmf.getTrustManagers(), null);
+	
+	    	// Tell the URLConnection to use a SocketFactory from our SSLContext
+	    	URL url = new URL("https://openwlanmap.org/android/upload.php");
+	    	c =(HttpsURLConnection)url.openConnection();
+	    	c.setSSLSocketFactory(context.getSocketFactory());    	
+	       
+	        c.setDoOutput(true); // enable POST
+	        c.setRequestMethod("POST");
+	        c.addRequestProperty("Content-Type","application/x-www-form-urlencoded, *.*");
+	        c.addRequestProperty("Content-Length",""+outString.length());
+	        os = new BufferedOutputStream(c.getOutputStream());
+	        os.write(outString.getBytes(),0,outString.length());
+	        os.flush();
+	        os.close();
+	        outString=null;
+	        os=null;
+	        System.gc();
+	         
+	         rc = c.getResponseCode();
+	         if (rc != HttpURLConnection.HTTP_OK) 
+	         {
+	            if (!silent)
+	             OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getString(R.string.http_error)+" "+rc);
+	            return false;
+	         }         
+	         is = new DataInputStream(c.getInputStream());         
+	         try
+	         {
+	            inString=is.readLine();
+	            remoteVersion=Integer.parseInt(inString);
+	            inString=is.readLine();
+	            scanData.uploadedCount=Integer.parseInt(inString);
+	            inString=is.readLine();
+	            scanData.uploadedRank=Integer.parseInt(inString);
+	
+	            inString=is.readLine();
+	            newAPs=Integer.parseInt(inString);
+	            inString=is.readLine();
+	            updAPs=Integer.parseInt(inString);
+	            inString=is.readLine();
+	            delAPs=Integer.parseInt(inString);
+	            inString=is.readLine();
+	            newPoints=Integer.parseInt(inString);
+	         }
+	         catch (NumberFormatException nfe)
+	         {
+	            nfe.printStackTrace();
+	            throw new IOException("upload.php failed");
+	         }
+	         
+	         is.close();
+	         is=null;
+	         ctx.deleteFile(OWMiniAtAndroid.WSCAN_FILE);
+	         ctx.deleteFile(OWMiniAtAndroid.WFREI_FILE);
+	ctx.deleteFile("wopendata");
+	         ScanService.scanData.setStoredValues(0);
+	         ScanService.scanData.setFreeHotspotWLANs(0);
+	         ctx.storeConfig();
+	         if (!silent)
+	         {
+	            if (remoteVersion>version)
+	             OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getText(R.string.new_version_available));
+	         }
+	      }
+	      catch (Exception e)
+	      {
+	         if (!silent)
+	          OWMiniAtAndroid.sendMessage(ScannerHandler.MSG_SIMPLE_ALERT,0,0,ctx.getResources().getText(R.string.upload_problem)+"\r\n"+e.getLocalizedMessage());
+	         uploadSuccess=false;
+	      }
+	      finally
+	      {
+	         try 
+	         {
+	            if (is != null) is.close();
+	            if (os != null) os.close();
+	            if (c != null) c.disconnect();
+	         }
+	         catch (IOException ioe)
+	         {
+	            ioe.printStackTrace();
+	         } 
+	      }      
       }
-      finally
-      {
-         try 
-         {
-            if (is != null) is.close();
-            if (os != null) os.close();
-            if (c != null) c.disconnect();
-         }
-         catch (IOException ioe)
-         {
-            ioe.printStackTrace();
-         } 
-      }      
       if (uploadSuccess)
       {
          if (!silent)
@@ -404,5 +505,5 @@ class UploadThread extends Thread
       }
       return uploadSuccess;
    }
-   
+  
 }
